3962da12371b1d9141592517aea16a684fdcb76a
John T. Foster <johntfosterjr@gmail.com>
2023-08-28 19:33:41 -0500

    Add teaching portfolio

--- 410c25c9a9cf2c18866604919c4298c007114cb8/statements/clean_table.sh
+++ 410c25c9a9cf2c18866604919c4298c007114cb8/statements/clean_table.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
 sed -e 's/\$/\\$/g' table.tex | \
-sed -e '26s/^/\\midrule/g' | \
-sed -e '27s/^/\\cellcolor{Gray}/g' | \
-sed -e '28s/^/\\cellcolor{Gray!40}/g' | \
-sed -e '15,25s/^/\\rowcolor{Gray} /g' | \
+sed -e '28s/^/\\midrule /g' | \
+sed -e '29s/^/\\cellcolor{Gray}/g' | \
+sed -e '30s/^/\\cellcolor{Gray!40}/g' | \
+sed -e '15,27s/^/\\rowcolor{Gray} /g' | \
 sed -e '14s/^/\\rowcolor{Gray!40} /g' | \
 sed -e '12s/^/\\rowcolor{Gray!40} /g' | \
 sed -e '1,3d;$d'
